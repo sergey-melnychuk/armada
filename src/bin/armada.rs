c43cfd4c2af587c99f57aaefdd6c2ca58a02918c
Sergey Melnychuk <serg.meln@gmail.com>
2023-05-16 00:51:47 +0200

    feat(bin): binary draft

--- src/bin/armada.rs
+++ src/bin/armada.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("It works!");
+use std::net::SocketAddr;
+
+use armada::ctx::Context;
+
+#[tokio::main]
+async fn main() {
+    let ctx = Context {};
+    let addr = SocketAddr::from(([0, 0, 0, 0], 9000));
+    armada::rpc::serve(addr, ctx).await
 }
