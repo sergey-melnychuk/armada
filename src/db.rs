b1b4fa703dabe8acde9659a156ad761eeaa6283a
Sergey Melnychuk <serg.meln@gmail.com>
2023-05-20 02:52:12 +0200

    feat(db): improve local storage

--- src/db.rs
+++ src/db.rs
@@ -34,6 +34,7 @@ impl Storage {
 
 pub trait Repo<T: Serialize + DeserializeOwned> {
     fn new(base: &Path) -> Self;
+    fn has(&self, key: &str) -> anyhow::Result<bool>;
     fn get(&self, key: &str) -> anyhow::Result<Option<T>>;
     fn del(&self, key: &str) -> anyhow::Result<Option<T>>;
     fn put(&self, key: &str, val: T) -> anyhow::Result<()>;
@@ -43,6 +44,29 @@ pub trait Repo<T: Serialize + DeserializeOwned> {
 pub struct DirRepo<T: Serialize + DeserializeOwned> {
     base: PathBuf,
     _phantom: PhantomData<T>,
+}
+
+impl<T> DirRepo<T>
+where
+    T: Serialize + DeserializeOwned,
+{
+    fn path(&self, key: &str) -> PathBuf {
+        let mut path = self.base.clone();
+        path.push(format!("{}.json", key));
+        path
+    }
+
+    fn file(&self, key: &str) -> anyhow::Result<Option<String>> {
+        let path = self.path(key);
+        if !path.exists() {
+            return Ok(None);
+        }
+
+        let mut file = File::open(&path)?;
+        let mut json = String::with_capacity(1024);
+        let _ = file.read_to_string(&mut json)?;
+        Ok(Some(json))
+    }
 }
 
 impl<T> Repo<T> for DirRepo<T>
@@ -58,47 +82,32 @@ where
         }
     }
 
+    fn has(&self, key: &str) -> anyhow::Result<bool> {
+        Ok(self.path(key).exists())
+    }
+
     fn get(&self, key: &str) -> anyhow::Result<Option<T>> {
-        let mut path = self.base.clone();
-        path.push(format!("{}.json", key));
-        if !path.exists() {
-            return Ok(None);
+        let json = self.file(key)?;
+        if let Some(json) = json {
+            let val: T = serde_json::from_str(&json)?;
+            return Ok(Some(val));
         }
-
-        let mut file = File::open(&path)?;
-
-        let mut json = String::with_capacity(1024);
-        let _ = file.read_to_string(&mut json)?;
-
-        let val: T = serde_json::from_str(&json)?;
-        Ok(Some(val))
+        Ok(None)
     }
 
     fn del(&self, key: &str) -> anyhow::Result<Option<T>> {
-        let mut path = self.base.clone();
-        path.push(format!("{}.json", key));
-        if !path.exists() {
-            return Ok(None);
+        let opt = self.get(key)?;
+        if opt.is_some() {
+            fs::remove_file(self.path(key))?;
         }
-
-        let mut file = File::open(&path)?;
-
-        let mut json = String::with_capacity(1024);
-        let _ = file.read_to_string(&mut json)?;
-
-        let val: T = serde_json::from_str(&json)?;
-        fs::remove_file(path)?;
-        Ok(Some(val))
+        Ok(opt)
     }
 
     fn put(&self, key: &str, val: T) -> anyhow::Result<()> {
-        let mut path = self.base.clone();
-        path.push(format!("{}.json", key));
-        let mut file = File::create(&path)?;
-
+        let path = self.path(key);
+        let mut file = File::create(path)?;
         let json = serde_json::to_string(&val)?;
         file.write_all(json.as_bytes())?;
-
         Ok(())
     }
 }
